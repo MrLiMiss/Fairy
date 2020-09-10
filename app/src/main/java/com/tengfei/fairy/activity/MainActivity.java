b1a65f6ab7eebcd8023c7b9d7cee998002a06fc2
tengfei.li <tengfei.li@beebank.com>
2020-09-10 18:00:00 +0800

    Task Fragnment点击波纹效果去除，tablayout 设置正确图片

--- app/src/main/java/com/tengfei/fairy/activity/MainActivity.java
+++ app/src/main/java/com/tengfei/fairy/activity/MainActivity.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentPagerAdapter;
 import androidx.viewpager.widget.PagerAdapter;
---
+++ app/src/main/java/com/tengfei/fairy/wedget/CustomViewPager.java
@@ -0,0 +1,40 @@
+package com.tengfei.fairy.wedget;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+
+import androidx.viewpager.widget.ViewPager;
+
+/**
+ * @ Description :  ViewPager禁止拖动
+ * @ Author 李腾飞
+ * @ Time 2020-09-10   17:24
+ * @ Version :
+ */
+public class CustomViewPager extends ViewPager {
+
+    private boolean isPagingEnabled = true;
+
+    public CustomViewPager(Context context) {
+        super(context);
+    }
+
+    public CustomViewPager(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        return this.isPagingEnabled && super.onTouchEvent(event);
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent event) {
+        return this.isPagingEnabled && super.onInterceptTouchEvent(event);
+    }
+
+    public void setPagingEnabled(boolean b) {
+        this.isPagingEnabled = b;
+    }
+}
