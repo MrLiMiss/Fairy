d591a44c9bb47ad4dd91ccad3d15c61a96ca5ed0
tengfei.li <tengfei.li@beebank.com>
2020-11-18 16:48:51 +0800

    添加未add文件

--- app/src/main/java/com/tengfei/fairy/wedget/myview/MyRefreshLayout.java
+++ app/src/main/java/com/tengfei/fairy/wedget/myview/MyRefreshLayout.java
@@ -1,10 +1,16 @@
 package com.tengfei.fairy.wedget.myview;
 
+import android.content.Context;
+import android.widget.LinearLayout;
+
 /**
- * @ Description :
+ * @ Description :自定义下拉刷新View
  * @ Author 李腾飞
  * @ Time 2020-11-18   15:49
  * @ Version :
  */
-public class MyRefreshLayout {
+public class MyRefreshLayout extends LinearLayout {
+    public MyRefreshLayout(Context context) {
+        super(context);
+    }
 }
