3044d363845d4292204c6a278718555681e0dd02
tengfei.li <tengfei.li@beebank.com>
2020-12-14 17:42:51 +0800

    新增校验身份证信息，城市 工具类，sqllite 测试勒

---
+++ app/src/main/java/com/tengfei/fairy/dataSQL/sqlTest/SqlLiteTest.java
@@ -0,0 +1,10 @@
+package com.tengfei.fairy.dataSQL.sqlTest;
+
+/**
+ * @ Description :Android 自带原生数据库test
+ * @ Author 李腾飞
+ * @ Time 2020/12/11   10:39
+ * @ Version :
+ */
+class SqlLiteTest {
+}
---
+++ app/src/main/java/com/tengfei/fairy/test/TestClass.java
@@ -0,0 +1,11 @@
+package com.tengfei.fairy.test;
+
+/**
+ * @ Description :
+ * @ Author 李腾飞
+ * @ Time 2020/12/12   10:41
+ * @ Version :
+ */
+class TestClass {
+
+}
