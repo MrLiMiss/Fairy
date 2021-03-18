ae793925ec13ac2b2ba84d099e2a5de4dc0ecbaf
tengfei.li <tengfei.li@beebank.com>
2021-03-18 14:19:26 +0800

    task 去除代码

--- app/src/main/java/com/tengfei/fairy/utils/Logs.java
+++ app/src/main/java/com/tengfei/fairy/utils/Logs.java
@@ -17,10 +17,6 @@ import java.util.Locale;
 /**
  * 日志管理工具类
  *
- * @author 刘国山 lgs@yitong.com.cn
- * @version 1.0 2012-8-15
- * @Description
- * @class com.yitong.zjrc.mbank.android.logs.Logs
  */
 public class Logs {
 
@@ -29,23 +25,6 @@ public class Logs {
     private static final boolean INFO = true;
     private static final boolean WARN = true;
     private static final boolean ERROR = true;
-
-
-    public static void v(String mess) {
-        if (BuildConfig.DEBUG) { Log.v(getTag(), buildMessage(mess)); }
-    }
-    public static void d(String mess) {
-        if (BuildConfig.DEBUG) { Log.d(getTag(), buildMessage(mess)); }
-    }
-    public static void i(String mess) {
-        if (BuildConfig.DEBUG) { Log.i(getTag(), buildMessage(mess)); }
-    }
-    public static void w(String mess) {
-        if (BuildConfig.DEBUG) { Log.w(getTag(), buildMessage(mess)); }
-    }
-    public static void e(String mess) {
-        if (BuildConfig.DEBUG) { Log.e(getTag(), buildMessage(mess)); }
-    }
 
     private static String getTag() {
         StackTraceElement[] trace = new Throwable().fillInStackTrace()
@@ -63,21 +42,6 @@ public class Logs {
         return callingClass;
     }
 
-    private static String buildMessage(String msg) {
-        StackTraceElement[] trace = new Throwable().fillInStackTrace()
-                .getStackTrace();
-        String caller = "";
-        for (int i = 2; i < trace.length; i++) {
-            Class<?> clazz = trace[i].getClass();
-            if (!clazz.equals(Logs.class)) {
-                caller = trace[i].getMethodName();
-                break;
-            }
-        }
-        return String.format(Locale.US, "[%d] %s: %s", Thread.currentThread()
-                .getId(), caller, msg);
-    }
-
     // public static final String TAG_PREFIX = "elife";
     public static boolean isShow = BuildConfig.DEBUG;
 
@@ -87,11 +51,6 @@ public class Logs {
         }
     }
 
-    public static void v(String tag, String msg, Throwable tr) {
-        if (VERBOSE&isShow) {
-            android.util.Log.v(tag, msg, tr);
-        }
-    }
 
     public static void d(String tag, String msg) {
         if (DEBUG&isShow) {
@@ -99,11 +58,6 @@ public class Logs {
         }
     }
 
-    public static void d(String tag, String msg, Throwable tr) {
-        if (DEBUG&isShow) {
-            android.util.Log.d(tag, msg, tr);
-        }
-    }
 
     public static void i(String tag, String msg) {
         if (INFO&isShow) {
@@ -111,29 +65,9 @@ public class Logs {
         }
     }
 
-    public static void i(String tag, String msg, Throwable tr) {
-        if (INFO&isShow) {
-            android.util.Log.i(tag, msg, tr);
-        }
-    }
 
-    public static void w(String tag, String msg) {
-        if (WARN&isShow) {
-            android.util.Log.w(tag, msg);
-        }
-    }
 
-    public static void w(String tag, String msg, Throwable tr) {
-        if (WARN&isShow) {
-            android.util.Log.w(tag, msg, tr);
-        }
-    }
 
-    public static void w(String tag, Throwable tr) {
-        if (WARN&isShow) {
-            android.util.Log.w(tag, tr);
-        }
-    }
 
     public static void e(String tag, String msg) {
         if (ERROR&isShow) {
