f4abfc0d8536d8ff8d4dfdb476ec2635a1524aad
tengfei.li <tengfei.li@beebank.com>
2021-06-28 10:24:36 +0800

    task 迁移androidx

---
+++ app/src/main/java/com/tengfei/fairy/utils/PacelableUtils.java
@@ -0,0 +1,11 @@
+package com.tengfei.fairy.utils;
+
+/**
+ * @ Description :android 序列化 Pacelable 相关工具类
+ * @ Author 李腾飞
+ * @ Time 2021/6/23   17:03
+ * @ Version :
+ */
+public class PacelableUtils {
+
+}
