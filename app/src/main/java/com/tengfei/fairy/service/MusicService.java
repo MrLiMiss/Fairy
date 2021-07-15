99c5468e7f3794a1ed8b1f4f73e941317a3bddb3
tengfei.li <tengfei.li@beebank.com>
2021-07-15 16:43:39 +0800

    task  测试音乐service

--- app/src/main/java/com/tengfei/fairy/service/MusicService.java
+++ app/src/main/java/com/tengfei/fairy/service/MusicService.java
@@ -1,12 +1,22 @@
 package com.tengfei.fairy.service;
 
 import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.res.AssetFileDescriptor;
+import android.content.res.AssetManager;
+import android.media.MediaPlayer;
 import android.os.Binder;
 import android.os.IBinder;
 import android.util.Log;
 
 import androidx.annotation.Nullable;
+
+import com.tengfei.fairy.activity.MainActivity;
+
+import java.io.IOException;
 
 /**
  * @ Description :自定义音乐播放器service
@@ -14,28 +24,68 @@ import androidx.annotation.Nullable;
  * @ Time 2021/7/9   17:16
  * @ Version :
  */
-public class MusicService  extends Service {
+public class MusicService extends Service {
 
-    public static final String TAG=MusicService.class.getSimpleName();
+    public static final String TAG = MusicService.class.getSimpleName();
+
+
+    public int current = 0;//当前播放的音乐
+
+    AssetManager assetManager;//内部asset文件管理器
+
+    String[] musics = new String[]{"任舒瞳_NotAngry.mp3", "熊同学_summertime.mp3", "孙昊天_卡农.mp3"};
+
+    int status = 0x11; // 当前的状态，0x11代表没有播放；0x12代表正在播放；0x13代表暂停
+
+    MediaPlayer mediaPlayer;
+
+    MyReceiver myReceiver;
 
     private SimpleBinder mBinder;
 
     @Override
     public void onCreate() {
         super.onCreate();
-        Log.d(TAG,"onCreate()");
+        Log.d(TAG, "onCreate()");
         Log.e(TAG, "MusicService-----" + Thread.currentThread().getName() + "--" + Thread.currentThread().getId());
-        mBinder=new SimpleBinder();
+
+        mBinder = new SimpleBinder();
+
+        assetManager = getAssets();
+        myReceiver = new MyReceiver();
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(ServiceTestActivity.CTL_ACTION);
+        registerReceiver(myReceiver, filter);
+        //创建MediaPlayer
+        mediaPlayer = new MediaPlayer();
+        //为MediaPlayer 播放完成时间绑定监听器
+        mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                current++;
+                if (current >= 3) {
+                    current = 0;
+                }
+                //发送广播通知Activity更改文本框
+                Intent sendIntent = new Intent(ServiceTestActivity.UPDATE_ACTION);
+                sendIntent.putExtra("current", current);
+                // 发送广播，将被Activity组件中的BroadcastReceiver接收到
+                sendBroadcast(sendIntent);
+                // 准备并播放音乐
+                prepareAndPlay(musics[current]);
+
+            }
+        });
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {//service 启动时呗调用
-        Log.d(TAG,"onStartCommand()");
+        Log.d(TAG, "onStartCommand()");
         new Thread(new Runnable() {//service 处于主线程，但是可在service中起子线程，完成耗时操作。
             @Override
             public void run() {
                 // 耗时任务
-                Log.d(TAG,"onStartCommand-子线程");
+                Log.d(TAG, "onStartCommand-子线程");
             }
         }).start();
         return super.onStartCommand(intent, flags, startId);
@@ -73,9 +123,90 @@ public class MusicService  extends Service {
                 @Override
                 public void run() {
                     //子线程耗时操作。
-                    Log.d(TAG,"SimpleBinder-子线程");
+                    Log.d(TAG, "SimpleBinder-子线程");
+
                 }
             }).start();
         }
     }
+
+
+    /**
+     * 广播接收器接收 ServiceTestActivity 的广播
+     */
+    public class MyReceiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(final Context context, Intent intent) {
+            int control = intent.getIntExtra("control", -1);
+            switch (control) {
+                // 播放或暂停
+                case 1:
+                    // 原来处于没有播放状态
+                    if (status == 0x11) {
+                        // 准备并播放音乐
+                        prepareAndPlay(musics[current]);
+                        status = 0x12;
+                    } else if (status == 0x12) {// 原来处于播放状态
+                        return;
+                    } else if (status == 0x13) {// 原来处于暂停状态
+                        // 播放
+                        mediaPlayer.start();
+                        // 改变状态
+                        status = 0x12;
+                    }
+                    break;
+                // 停止声音
+                case 2:
+                    // 如果原来正在播放或暂停
+                    if (status == 0x12 || status == 0x13) {
+                        // 停止播放
+                        mediaPlayer.stop();
+                        status = 0x11;
+                    }
+                break;
+                case 3:
+                    current--;
+                    if (current < 0)
+                        current = 0;
+                    prepareAndPlay(musics[current]);
+                    break;
+                case 4:
+                    current++;
+                    if (current > musics.length - 1)
+                        current = 0;
+                    prepareAndPlay(musics[current]);
+                    break;
+            }
+            // 广播通知Activity更改图标、文本框
+            Intent sendIntent = new Intent(ServiceTestActivity.UPDATE_ACTION);
+            sendIntent.putExtra("update", status);
+            sendIntent.putExtra("current", current);
+            // 发送广播，将被Activity组件中的BroadcastReceiver接收到
+            sendBroadcast(sendIntent);
+        }
+    }
+
+
+    /**
+     * 播放音乐
+     *
+     * @param music
+     */
+    private void prepareAndPlay(String music) {
+        try {
+            // 打开指定音乐文件
+            AssetFileDescriptor afd = assetManager.openFd(music);
+            mediaPlayer.reset();
+            // 使用MediaPlayer加载指定的声音文件。
+            mediaPlayer.setDataSource(afd.getFileDescriptor(),
+                    afd.getStartOffset(), afd.getLength());
+            // 准备声音
+            mediaPlayer.prepare();
+            // 播放
+            mediaPlayer.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
--- app/src/main/java/com/tengfei/fairy/service/ServiceTestActivity.java
+++ app/src/main/java/com/tengfei/fairy/service/ServiceTestActivity.java
@@ -1,13 +1,20 @@
 package com.tengfei.fairy.service;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.tengfei.fairy.R;
 import com.tengfei.fairy.base.BaseActivity;
 import com.tengfei.fairy.utils.IntentUtils;
+import com.tengfei.fairy.utils.Logs;
 
+import butterknife.BindView;
 import butterknife.OnClick;
 
 /**
@@ -17,14 +24,39 @@ import butterknife.OnClick;
  * @ Version :
  */
 public class ServiceTestActivity extends BaseActivity {
+
+
+    public static final String CTL_ACTION="org.crazyit.action.CTL_ACTION";
+    public static final String UPDATE_ACTION="org.crazyit.action.UPDATE_ACTION";
+
+    @BindView(R.id.btn_startmusic)
+    Button btn_start_music;//开始音乐
+    @BindView(R.id.btn_suspendmusic)
+    Button btn_suspend_music;//暂停音乐
+
+    String[] titleStrs = new String[] { "NotAngry", "卡农", "summertime" };
+    String[] authorStrs = new String[] { "任舒瞳", "孙昊天", "熊同学" };
+
+    //ox开头表示十六进制，0开头表示8进制
+    public int status = 0x11;//定义音乐播放状态 0x11 未播放，0x12 正在播放，0x13 暂停播放
+
+    MusicReceiver musicReceiver;
+
+    @BindView(R.id.tv_music_name)
+    TextView tv_music_name;
+    @BindView(R.id.tv_music_singer)
+    TextView tv_music_singer;
+
     public static String TAG=ServiceTestActivity.class.getSimpleName();
+
     @Override
     protected int getContentLayout() {
         return R.layout.activity_service;
     }
 
-    @OnClick({R.id.btn_start_service,R.id.btn_end_service,R.id.btn_bind_service,R.id.btn_unbind_service,R.id.btn_start_intent_service,R.id.btn_end_intent_service})
+    @OnClick({R.id.btn_start_service,R.id.btn_end_service,R.id.btn_bind_service,R.id.btn_unbind_service,R.id.btn_start_intent_service,R.id.btn_end_intent_service,R.id.btn_startmusic,R.id.btn_suspendmusic,R.id.btn_nextmusic,R.id.btn_premusic})
     void click(View view){
+        Intent intent =new Intent("org.crazyit.action.CTL_ACTION");
        switch (view.getId()){
            case R.id.btn_start_service://启动service
                IntentUtils.startMusicService(this);
@@ -44,7 +76,27 @@ public class ServiceTestActivity extends BaseActivity {
            case R.id.btn_end_intent_service:
                IntentUtils.endIntentService(this);
                break;
-
+           case R.id.btn_startmusic:
+               Logs.d(TAG,"开始音乐");
+               intent.putExtra("control", 1);
+               // 发送广播，将被Service组件中的BroadcastReceiver接收到
+               sendBroadcast(intent);
+               break;
+           case R.id.btn_suspendmusic:
+               intent.putExtra("control", 2);
+               sendBroadcast(intent);
+               Logs.d(TAG,"暂停音乐");
+               break;
+           case R.id.btn_nextmusic:
+               intent.putExtra("control", 3);
+               sendBroadcast(intent);
+               Logs.d(TAG,"下一首音乐");
+               break;
+           case R.id.btn_premusic:
+               intent.putExtra("control", 4);
+               sendBroadcast(intent);
+               Logs.d(TAG,"上一首音乐");
+               break;
 
            default:
                break;
@@ -59,10 +111,64 @@ public class ServiceTestActivity extends BaseActivity {
     @Override
     protected void initAction() {
 
+        musicReceiver=new MusicReceiver();
+        // 创建IntentFilter，动态注册Brocasteceiver  无需AndroidMainfest.xml 再次注册
+        IntentFilter filter = new IntentFilter();
+        // 指定BroadcastReceiver监听的Action
+        filter.addAction(UPDATE_ACTION);
+        // 注册BroadcastReceiver
+        registerReceiver(musicReceiver, filter);
+
+        Intent intent = new Intent(this, MusicService.class);
+        // 启动后台Service
+        startService(intent);
+
+
     }
 
     @Override
     protected void initData() {
 
+    }
+
+
+    /**
+     * 广播接收器
+     */
+    public class MusicReceiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            // 获取Intent中的update消息，update代表播放状态
+            int update = intent.getIntExtra("update", -1);
+            // 获取Intent中的current消息，current代表当前正在播放的歌曲
+            int current = intent.getIntExtra("current", -1);
+            if (current >= 0)
+            {
+                tv_music_name.setText(titleStrs[current]);
+                tv_music_singer.setText(authorStrs[current]);
+            }
+            switch (update)
+            {
+                case 0x11:
+//                    play.setImageResource(R.drawable.play);
+                    status = 0x11;
+                    break;
+                // 控制系统进入播放状态
+                case 0x12:
+                    // 播放状态下设置使用暂停图标
+//                    play.setImageResource(R.drawable.pause);
+                    // 设置当前状态
+                    status = 0x12;
+                    break;
+                // 控制系统进入暂停状态
+                case 0x13:
+                    // 暂停状态下设置使用播放图标
+//                    play.setImageResource(R.drawable.play);
+                    // 设置当前状态
+                    status = 0x13;
+                    break;
+            }
+
+        }
     }
 }
