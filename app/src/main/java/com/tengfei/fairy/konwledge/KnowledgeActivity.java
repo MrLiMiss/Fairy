0f9b9cbfcba0dd1762244b9a7ea91594b8d5adbe
litengfei <tengfei.li@bjrxtd.com>
2021-12-07 16:06:36 +0800

    task Html.formHtml()完成字符变化操作

--- app/src/main/java/com/tengfei/fairy/konwledge/KnowledgeActivity.java
+++ app/src/main/java/com/tengfei/fairy/konwledge/KnowledgeActivity.java
@@ -3,6 +3,7 @@ package com.tengfei.fairy.konwledge;
 import android.os.Build;
 import android.text.Html;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.annotation.RequiresApi;
@@ -23,9 +24,9 @@ import butterknife.OnClick;
  */
 public class KnowledgeActivity extends BaseMvpActivity<BasePresenter> {
     @BindView(R.id.tv_html_forhtml1)
-    TextView mTvHtmlForHtml1;
-    @BindView(R.id.tv_html_forhtml2)
-    TextView mTvHtmlForHtml2;
+    TextView mTvHtmlForHtml;
+    @BindView(R.id.et_html_forhtml2)
+    EditText mEtHtmlForHtml2;
 
     public static String TAG = KnowledgeActivity.class.getSimpleName();
 
@@ -49,19 +50,35 @@ public class KnowledgeActivity extends BaseMvpActivity<BasePresenter> {
         }
     }
 
+
+
     /**
+     * 1、Html.fromHtml Android 不支持size标签的解析，即不能通过默认string完成字体大小的哦哦控制
+     * 2、Html.fromHtml(String) 已过时
+     * 3、html 格式的 String 如果存储在String.xml 文件中需要转义HTML标签，不然的话 经过Android处理后 所有的HTML标签都给过滤掉了(需要把所有的“<”用“&lt;”替换)。
+     * 这种情况下可以使用XML的CDATA标记:
+     *  <string name="anti_fraud_type_2"><Data><![CDATA[     <font color="#999999" size="30px">类型：</font><font color="#FF6D26" size="30px">%1$s</font>    ]]></Data></string>
+     *
      * 测试Html.forHtml()
+     *
      */
     private void detailHtml() {
+        String htmlStr2 =
+                "<span style='color:#EE30A7;font-size:20px'>Html" +
+                        "<font color='#EE2C2C' size='40px'>字体变大,色值变化</font>"
+                        +
+                        "<font color='#CD8500' size='60px'>字体变大,色值变化1</font>" +
+                        "</span>";
+
         String strHtml = "<font color=\"#EE2C2C\"size=\"40px\">字体变大,色值变化</font>"
-                        + "<font color=\"#CD8500\"size=\"60px\">字体变大,色值又变化1</font>" + "</span>";
-        mTvHtmlForHtml1.setText(Html.fromHtml(strHtml));
+                        + "<font color=\"#CD8500\"size=\"80px\">字体变大,色值也变化了</font>" + "</span>";
+        mTvHtmlForHtml.setText(Html.fromHtml(htmlStr2));
 
         String strHtml2 =
                 "<font color='#4F94CD' size='40px'>我已经完成</font>" +
                         "<font color='#FF0000' size='80px'>80%</font>" +
                         "<font color='#4F94CD' size='40px'>的暑假作业</font>";
-        mTvHtmlForHtml2.setText(Html.fromHtml(strHtml2));
+        mEtHtmlForHtml2.setText(Html.fromHtml(strHtml2));
 
     }
 
