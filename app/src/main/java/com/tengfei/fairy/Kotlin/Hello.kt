bbe5d2db322921cf6ec2b66f3562fe4d5bee427f
tengfei.li <litengfei512@163.com>
2022-08-01 09:48:05 +0800

    Task  kotlin hello word

---
+++ app/src/main/java/com/tengfei/fairy/Kotlin/Hello.kt
@@ -0,0 +1,13 @@
+package com.tengfei.fairy.Kotlin
+
+/**
+ * @Description :
+ * @Auther tengfei.li
+ * @Since 2022/7/29 17:56
+ * @Version 1.0
+ */
+class Hello {}
+
+fun main(args: Array<String>) {    // 包级可见的函数，接受一个字符串数组作为参数
+    println("Hello World!")         // 分号可以省略
+}
